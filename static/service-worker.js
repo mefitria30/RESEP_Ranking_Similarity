a0158496d00ef4bc9f9090104ed35a77525a1424
Fitria Melliyanni <fitria.melliyanni@dataon.com>
2026-04-25 07:48:41 +0700

    update req

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = "resep-cache-v6";
+const CACHE_NAME = "resep-cache-v11";
 const urlsToCache = [
-    "/",
-    "/offline.html",
+    "/",                // halaman utama (home + result)
+    "/offline.html",    // fallback offline
     "/static/manifest.json",
     "/static/icons/icon-192.png",
     "/static/icons/icon-512.png",
     "/static/recipe_vectors.json",
-    "/static/assets/bootstrap.bundle.min.js",
     "/static/assets/bootstrap.min.css",
+    "/static/assets/bootstrap.bundle.min.js",
     "/static/assets/jquery-3.6.0.min.js",
     "/static/assets/jquery-ui.css",
     "/static/assets/jquery-ui.js",
+    "/static/recommend.js"
 ];
 
 self.addEventListener( "install", event =>
@@ -29,3 +30,13 @@ self.addEventListener( "fetch", event =>
         } )
     );
 } );
+
+caches.open( CACHE_NAME ).then( cache =>
+{
+    return Promise.all(
+        urlsToCache.map( url =>
+            cache.add( url ).catch( err => console.error( "Gagal cache:", url, err ) )
+        )
+    );
+} );
+
